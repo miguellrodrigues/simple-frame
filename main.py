bdc79243d11fc0af6e0f69b9ed2057fc6b1c61f1
Miguel L. Rodrigues <miguellukas52@gmail.com>
2021-11-14 20:19:57 -0300

    updates

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ f1.rotate_around_arbitrary_vector(1.57, [0, 1.2, 0])
 def update():
   f1.translate_to(f0)
 
-  r = np.abs(f1.rotation_to(f0))
+  r = f1.rotation_to(f0)
 
-  f1.rotate(0, -r[1]*.03, 0)
+  f1.rotate(0, r[1]*-.03, 0)
 
 
 drawer = FrameDrawer([f0, f1], update)
-drawer.show(True)
+drawer.show()
